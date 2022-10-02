df856a7616a8e96f6f88b4848d335ddc0db2e2c2
hw11chung <ddoldolry@gmail.com>
2022-10-02 03:34:17 -0700

    changed JS Random to match documentation

--- scripts.js
+++ scripts.js
@@ -1,6 +1,6 @@
 //Computer randomly returns Rock, Paper, or Scissors
 function getComputerChoice() {
-  let choice = Math.floor(Math.random() * 3 + 1); //random number between 1 and 3
+  let choice = Math.floor(Math.random() * 3) + 1; //random number between 1 and 3
   if (choice === 1) {
     return "Rock";
   } else if (choice === 2) {
@@ -34,4 +34,4 @@ const playerSelection = "rock";
 const computerSelection = getComputerChoice();
 console.log(playRound([playerSelection, computerSelection]));
 
-//console.log(getComputerChoice());
+console.log(getComputerChoice());
